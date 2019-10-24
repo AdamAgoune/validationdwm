0d30605edcd6d42dd593138a168ec0ed60fd24a1
francois2sap <francois@desaporta.fr>
2019-10-24 17:22:20 +0200

    validation1

---
+++ js/js.js
@@ -0,0 +1,18 @@
+document.querySelector(".bouton1").addEventListener("click", function() {  
+  
+    document.getElementById("texte1").classList.toggle("modif");
+}) ;
+document.querySelector(".bouton2").addEventListener("click", function() {  
+  
+    document.getElementById("texte2").classList.toggle("modif");
+}) ;
+document.querySelector(".bouton3").addEventListener("click", function() {  
+  
+    document.getElementById("texte3").classList.toggle("modif");
+}) ;
+document.querySelector(".bouton4").addEventListener("click", function() {  
+  
+    document.getElementById("texte4").classList.toggle("modif");
+}) ;
+
+
